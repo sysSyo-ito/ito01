410c6a13f60818c0fd8131cb67c9299156350fec
sysSito <w4abe54fd7f2b8@gmail.com>
2019-04-27 11:42:40 +0900

    test

---
+++ app/src/main/java/com/example/w4abe/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.w4abe.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt_tosubmenu = findViewById(R.id.受付_button);
+        bt_tosubmenu.setOnClickListener(new View.OnClickListener(){
+
+
+
+        @Override
+        public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClassName(getPackageName(),getPackageName()+".submenuActivity");
+                startActivity(intent);
+            }
+        });
+    }
+}
+
